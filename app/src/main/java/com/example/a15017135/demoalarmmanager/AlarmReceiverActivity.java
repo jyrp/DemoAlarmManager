e448fff5e92035665a2e4cb5112c51c184d8dd92
15017135 <15017135@myrp.edu.sg>
2017-05-30 10:33:01 +0800

    a

--- app/src/main/java/com/example/a15017135/demoalarmmanager/AlarmReceiverActivity.java
+++ app/src/main/java/com/example/a15017135/demoalarmmanager/AlarmReceiverActivity.java
@@ -1,8 +1,17 @@
 package com.example.a15017135.demoalarmmanager;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by 15017135 on 30/5/2017.
  */
 
-public class AlarmReceiverActivity {
-}
+public class AlarmReceiverActivity extends AppCompatActivity {
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_receiver);
+    }
+};
